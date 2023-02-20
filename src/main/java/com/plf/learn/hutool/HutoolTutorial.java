981f9df9588dfbc593fc5295197fd07e3c599e3d
panlf <liangfeng_pan@163.com>
2023-02-20 21:25:35 +0800

    hutool reflect

--- src/main/java/com/plf/learn/hutool/HutoolTutorial.java
+++ src/main/java/com/plf/learn/hutool/HutoolTutorial.java
@@ -3,9 +3,12 @@ package com.plf.learn.hutool;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.date.LocalDateTimeUtil;
+import cn.hutool.core.util.ReflectUtil;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
@@ -13,6 +16,16 @@ import java.util.Date;
  * @date 2022/12/19
  */
 public class HutoolTutorial {
+
+    @Test
+    public void testReflect(){
+       Class<?> aClass= InfoConstants.class;
+        Field[] fields = aClass.getFields();
+        Arrays.stream(fields).forEach(field -> {
+            System.out.println(ReflectUtil.getFieldName(field)+":"+ReflectUtil.getStaticFieldValue(field));
+        });
+
+    }
 
     @Test
     public void test(){
---
+++ src/main/java/com/plf/learn/hutool/InfoConstants.java
@@ -0,0 +1,11 @@
+package com.plf.learn.hutool;
+
+/**
+ * @author panlf
+ * @date 2023/2/20
+ */
+public class InfoConstants {
+    public static final String APPLE = "APPLE";
+    public static final int num = 5;
+    public static final String CHINA = "CHINA";
+}
